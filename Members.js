b2a414890608b38de58de1fca8e41de566a2774f
Bilalzafar07 <83906703+Bilalzafar07@users.noreply.github.com>
2021-06-05 03:02:05 -0700

    Add files via upload

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "Jhon Doe",
+    email: "jhon@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "bob Williams",
+    email: "bob@gmail.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "Shanon jackson",
+    email: "shanon@gmail.com",
+    status: "active",
+  },
+];
+
+module.exports = members;
